d7d329d01007240a186c61a4c2b1154cee78c8b2
gulsu <gulsu@gus>
2020-03-17 09:23:21 +0100

    essai

---
+++ LaChimie/src/LaChimie/src/Chimie/ActionHydrogène.java
@@ -0,0 +1,20 @@
+package Chimie;
+
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class ActionHydrogène extends JFrame implements ActionListener {
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+				
+		System.out.println("essai");
+	}
+
+}
